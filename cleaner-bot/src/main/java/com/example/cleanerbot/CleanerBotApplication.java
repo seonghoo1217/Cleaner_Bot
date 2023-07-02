319479e4cca0e3057e9bb5ff129702a40f45a607
seonghoo1217 <seonghoo1217@naver.com>
2023-07-02 20:39:12 +0900

    chore : Initial build setting

---
+++ cleaner-bot/src/main/java/com/example/cleanerbot/CleanerBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.cleanerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanerBotApplication.class, args);
+	}
+
+}
---
+++ cleaner-bot/src/test/java/com/example/cleanerbot/CleanerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cleanerbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanerBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
